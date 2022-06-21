683dc9677a8b95af90531d1f03b399c9b8770c3a
Wellington Witaski <wellingtonwitaski@hotmail.com>
2022-06-21 19:23:26 -0300

    Exercício Etec 2022

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 const handleSearch = async (event) => {
   event.preventDefault();
 
-  // implemente a consulta a partir daqui
+  const MessageUser = document.getElementById('message');
+  const ListaShows = document.getElementById('shows');
+  const nomeDigitado = document.getElementById('query').value;
+  const URL = `https://api.tvmaze.com/search/shows?q=${nomeDigitado}`;
+  const Resultado = await fetch(URL);
+  const ListaBuscada = await Resultado.json();
 
-  //// Exemplo de endpoint: https://api.tvmaze.com/search/shows?q=lost
+  MessageUser.innerHTML = '';
+  ListaShows.innerHTML = '';
 
-  //// Elementos de leiaute importantes:
+  if (!Resultado.ok) {
+    MessageUser.innerHTML = 'A aplicação falhou!';
+    return;
+  }
 
-  //  #message: use para exibir mensagens aos usuário, por exemplo:
+  if (ListaBuscada.length === 0) {
+    MessageUser.innerHTML = 'Não encontrado!';
+    return;
+  }
 
-  const message = document.querySelector('#message');
-  message.innerHTML = 'exercício ainda não resolvido.';
+  ListaBuscada.forEach((element) => {
+    const MostraNome = element?.show?.name;
+    const MostraImagem = element?.show?.image?.medium || '';
 
-  //  #shows: conterá os shows, cada um em um <li>, por exemplo:
-  // <li>
-  //   <img class="poster" src="https://static.tvmaze.com/uploads/images/medium_portrait/0/1389.jpg" />
-  //   <span class="show-name">Lost</span>
-  // </li>
+    ListaShows.insertAdjacentHTML(
+      'beforeend',
+      `
+      <li>
+          <img class="poster" src="${MostraImagem}" />
+          <span class="show-name">${MostraNome}</span>
+      </li>
+`
+    );
+  });
 };
 
 document.addEventListener('DOMContentLoaded', () => {
